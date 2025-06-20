69b47fd9e1483eb76a96501a04775b15625b2165
vwhernan <148294852+vwhernan@users.noreply.github.com>
2025-06-19 17:34:56 -0700

    Update Assignment5.cpp

--- Assignment5/Assignment5/Assignment5.cpp
+++ Assignment5/Assignment5/Assignment5.cpp
@@ -1,5 +1,15 @@
-// Assignment5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* ---------------------------------------------------------------------------
+** I attest that this following code represents my own work and is subject to
+** the plagiarism policy found in the course syllabus.
+** 
+** Class: 	CSC 242
+** Assignment: 	Lesson #5
+** File: 	Assignment5.cpp
+** Description: This is a test for assignment 3
+**
+** Author: 	Vincent Hernandez
+** Date: 	06/19/2025
+** -------------------------------------------------------------------------*/
 using namespace std;
 #include <iostream>
 
