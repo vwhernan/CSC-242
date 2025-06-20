f796f1d60883353aa54350c78d1941055fb7ab30
vwhernan <148294852+vwhernan@users.noreply.github.com>
2025-06-19 17:32:16 -0700

    Add files via upload

---
+++ Assignment5/Assignment5/Assignment5.cpp
@@ -0,0 +1,10 @@
+// Assignment5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+using namespace std;
+#include <iostream>
+
+int main()
+{
+    cout << "Hello World!\n";
+}
+
